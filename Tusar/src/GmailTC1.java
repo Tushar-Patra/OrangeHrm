f533109e8771f1e3dca93c0c094d2d14a27f4e67
USER <USER@192.168.0.4>
2019-06-10 17:52:52 +0530

    GmailTC1.java

---
+++ Tusar/src/GmailTC1.java
@@ -0,0 +1,24 @@
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class GmailTC1 
+{
+
+	public static void main(String[] args)
+	{
+		WebDriver driver=new FirefoxDriver();
+		driver.get("http://google.com");
+		driver.findElement(By.linkText("Gmail")).click();
+		driver.navigate().back();
+		driver.navigate().forward();
+		driver.navigate().refresh();
+		driver.close();
+		driver.quit();
+
+		
+	}
+
+}
